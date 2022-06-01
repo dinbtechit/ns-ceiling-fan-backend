adb2cafbf8df1d0466858e675f9339e5dc6d4fb0
dinesh <dinbtechit@gmail.com>
2022-06-01 12:57:14 -0700

    Basic Test cases

--- src/fan/fan.controller.spec.ts
+++ src/fan/fan.controller.spec.ts
@@ -1,22 +1,38 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { FanController } from './fan.controller';
 import { FanService } from './fan.service';
+import { RedisService } from '../redis/redis.service';
 
 describe('FanController', () => {
   let fanController: FanController;
+  let fanService: FanService;
 
   beforeEach(async () => {
     const app: TestingModule = await Test.createTestingModule({
       controllers: [FanController],
-      providers: [FanService],
+      providers: [FanService, RedisService],
     }).compile();
-
     fanController = app.get<FanController>(FanController);
+    fanService = app.get<FanService>(FanService);
   });
 
   describe('Fan APIs', () => {
     it('should return Fan status from redis"', () => {
-      expect(fanController.fanStatus()).toBe({ speed: 0, direction: true });
+      const value = { speed: 0, direction: true };
+      const result = new Promise(() => value);
+      jest
+        .spyOn(fanService, 'getFanStatus')
+        .mockReturnValueOnce(new Promise((resolve) => value));
+      expect(fanController.fanStatus()).toBeTruthy();
+    });
+
+    it('should return set fanState in Redis when cord1 is pulled', () => {
+      const value = { speed: 0, direction: true };
+      const result = new Promise(() => value);
+      jest
+        .spyOn(fanService, 'getFanStatus')
+        .mockReturnValueOnce(new Promise((resolve) => value));
+      expect(fanController.pullCord1()).toBeTruthy();
     });
   });
 });
